9037a998614ee3ced2e58afb358a8d7a7c015746
hugolfsimoes <hugolfsimoes@gmail.com>
2022-08-10 20:59:08 -0300

    feito as configuracoes iniciais e estilo

---
+++ src/components/TaskForm.tsx
@@ -0,0 +1,25 @@
+import React, { useState, ChangeEvent, FormEvent, useEffect } from 'react';
+import { ITask } from '../interfaces/ITask';
+import styles from './TaskForm.module.css';
+type Props = {
+  btnText: string;
+};
+
+const TaskForm = ({ btnText }: Props) => {
+
+  return (
+    <form className={styles.form}>
+      <div className={styles.input_container}>
+        <label htmlFor="title">Título:</label>
+        <input type="text" name='title' placeholder='Título da tarefa' />
+      </div>
+      <div className={styles.input_container}>
+        <label htmlFor="difficulty">Dificuldade:</label>
+        <input type="text" name='difficulty' placeholder='Dificuldade da tarefa' />
+      </div>
+      <input type="submit" value={btnText} />
+    </form>
+  );
+};
+
+export default TaskForm;
---
+++ src/components/TaskList.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './TaskList.module.css';
+
+type Props = {};
+
+const TaskList = (props: Props) => {
+  return (
+    <div>TaskList</div>
+  );
+};
+
+export default TaskList;
